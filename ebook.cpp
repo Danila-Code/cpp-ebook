407e2943cf10d94e4c0b13ef5fbd51225a825d56
Danila Danilov <danila.Zcode@gmail.com>
2025-07-24 09:28:08 +0300

    after review

--- ebook.cpp
+++ ebook.cpp
@@ -12,29 +12,32 @@ public:
         pages_readings_(MAX_PAGES_COUNT + 1, 0) {
     }
     
-    void Read(int user_id, int page) const {
+    void Read(int user_id, int page) {
         UpdatePagesReadings(users_progress_[user_id] + 1, page);
         users_progress_[user_id] = page;
     }
 
     double Cheer(int user_id) const {
-        if(users_progress_[user_id] == -1) {
+        bool is_user_read_no_pages = users_progress_[user_id] == -1;
+        if(is_user_read_no_pages) {
             return 0;
         }
+
         const int user_count = GetActiveUserCount();
-        return user_count == 1 ? 1 : 
-            1.0 - (pages_readings_[users_progress_[user_id]] - 1.0) 
-            / (user_count - 1.0);
+        double user_less_pages = user_count - pages_readings_[users_progress_[user_id]] - 1;
+        
+        return user_count == 1 ? 1 :
+                                 user_less_pages / (user_count - 1.0);
     }
 
 private:
     const size_t MAX_USER_COUNT = 100000;
     const size_t MAX_PAGES_COUNT = 1000;
 
-    mutable std::vector<int> users_progress_;
-    mutable std::vector<int> pages_readings_;
+    std::vector<int> users_progress_;
+    std::vector<int> pages_readings_;
 
-    void UpdatePagesReadings(int from_page, int to_page) const {
+    void UpdatePagesReadings(int from_page, int to_page) {
         for(int i = from_page; i <= to_page; ++i) {
             ++pages_readings_[i];
         }
@@ -66,21 +69,5 @@ void ManageEBook(EbookManager& ebook, std::istream& input, std::ostream& output)
 
 int main() {
     EbookManager ebook;
-
-    std::istringstream str_stream{R"(12
-        CHEER 5
-        READ 1 10
-        CHEER 1
-        READ 2 5
-        READ 3 7
-        CHEER 2
-        CHEER 3
-        READ 3 10
-        CHEER 3
-        READ 3 11
-        CHEER 3
-        CHEER 1)"
-    };
-
-    ManageEBook(ebook, str_stream, std::cout);
+    ManageEBook(ebook, std::cin, std::cout);
 }
